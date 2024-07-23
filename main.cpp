7ac9d6df1452ebd0384424a25f426ee48a921353
DavidIRL <david.white0911@gmail.com>
2024-07-23 01:16:03 -0400

    init repo && main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+enum class State {kEmpty, kObstacle};
+
+vector<State> ParseLine(string line) {
+    istringstream sline(line);
+    int n;
+    char c;
+    vector<State> row;
+    while (sline >> n >> c && c == ',') {
+        if (n == 0) {
+            row.push_back(State::kEmpty);
+        } else {
+            row.push_back(State::kObstacle);
+        }
+    }
+    
+    return row;
+}
+
+
